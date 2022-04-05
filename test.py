6c292359fb59f26c2bd936036a175a2949ae082b
RasmusCarlsen <78859677+Rasmus-M-C@users.noreply.github.com>
2022-04-05 13:50:50 +0200

    updated devices

--- test.py
+++ test.py
@@ -5,27 +5,26 @@ from time import sleep
 HOST = "127.0.0.1"
 PORT = 1883
 
-def __on_message(self, client, userdata, message) -> None:
-    payload = message.payload.decode("utf-8")
-    payload_recieve = json.loads(payload_recieve)
-    if(payload_recieve.topic=="0x00158d00057b4d2a"):
-        if(payload_recieve.occupancy==True):
-            self.publish(topic=f"zigbee2mqtt/0x60a423fffe023458/set",
-                    payload_send=json.dumps({"effect": "blink"}))
+def __on_message(client, userdata, message):
+    print("msg received")
+    if "SENSOR1" in message.topic:
+        payload_recieve = message.payload.decode("utf-8")
+        payload_recieve = json.loads(payload_recieve)
+        print(payload_recieve)
+        if(payload_recieve["occupancy"]==True):
+            client.publish(topic=f"zigbee2mqtt/NY_LED_WORKS_YES/set", payload=json.dumps({"effect": "blink"}))
 
 if __name__ == "__main__":
     # Create a data model and add a list of known Zigbee devices.
     mqtt_client= MqttClient()
   
     # Connect to the host given in initializer.
-    mqtt_client.on_connect=__on_connect
-    mqtt_client.on_subscribe = __on_subscribe
     mqtt_client.connect(HOST, PORT)
     mqtt_client.on_message = __on_message
     mqtt_client.loop_start()
     # Subscribe to all topics given in the initializer.
-    mqtt_client.subscribe("zigbee2mqtt/0x00158d00057b4d2a")
-    mqtt_client.subscribe("zigbee2mqtt/0x60a423fffe023458")
+    mqtt_client.subscribe("zigbee2mqtt/#")
+    mqtt_client.subscribe("zigbee2mqtt/NY_LED_WORKS_YES")
    
     # Start the subscriber thread.
    
