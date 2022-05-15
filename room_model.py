d14dab074b43c534c6757d3babec82eaead23893
Rasmus-M-C <rasmusmcarlsen@gmail.com>
2022-05-15 13:44:17 +0200

    fully working, no interval runtime + real sensors

--- room_model.py
+++ room_model.py
@@ -22,7 +22,7 @@ class Room():
   Id_LED: str
   Host: str
   Port: int
-  Room_visisted: int = 0
+  Room_visited: int = 0
   Internal_counter: int = 0
 
   def __init__(self,id_sensor, id_LED, Host, Port):
@@ -72,9 +72,9 @@ class Server():
     formatted_date = now.strftime('%Y-%m-%d %H:%M:%S') # Current time in SQL format
     
     cursor = self.mysql_conn.cursor()
-    insert_stmt = ("INSERT INTO bathroom_data (start_time, time_to_bathroom, time_in_bathroom, time_to_bedroom, timestamp)"
-     "VALUES (%s, %s, %s, %s, %s)") ## Table values
-    data = (timing["start_time"], timing["time_to_bathroom"], timing["time_in_bathroom"], timing["time_to_bedroom"], formatted_date)
+    insert_stmt = ("INSERT INTO bathroom_data (time_to_bathroom, time_in_bathroom, time_to_bedroom, timestamp)"
+     "VALUES (%s, %s, %s, %s)") ## Table values
+    data = (timing["time_to_bathroom"], timing["time_in_bathroom"], timing["time_to_bedroom"], formatted_date)
     cursor.execute(insert_stmt, data)
     self.mysql_conn.commit()
     print("Logged bathroom event")
@@ -91,31 +91,44 @@ class Controller(): # changes model aka rooms
     self.MYSQL_Server = input_server
   
   
+
+
+  
   ####################################################################################
   ######den udregner time_to_bathroom forkert, time_in_bathroom, time_to_bedroom######
   ####################################################################################
   def bathroom_event(self, room_list: list, timing: dict): ## den udregner time_to_bathroom forkert, time_in_bathroom, time_to_bedroom######
+    
     room_list_len = len(room_list)-1
-    print(room_list[0].Room_visisted - int(time.time()) < room_list[-1].Room_visisted - int(time.time()),"TEST STATEMENT 1")
-    if(room_list[0].Room_visisted - int(time.time()) < room_list[-1].Room_visisted - int(time.time())): # vi kommer fra soveværelset
+    if(room_list[0].Room_visited - int(time.time()) > room_list[-1].Room_visited - int(time.time()) and room_list[-1].Room_visited): # vi kommer fra soveværelset
       for index, room in enumerate(room_list):
         if(index != room_list_len):
-          if(room_list[index].Room_visisted != 0 and room_list[index+1].Room_visisted - room.Room_visisted < 300 and room_list[index+1].Room_visisted > room.Room_visisted): # Huske at vurdere der ikke er gået for lang tid siden man gik i midterste rum 
-           pass            
-        elif(room.Room_visisted != 0):
-          timing["start_time"] = room_list[0].Room_visisted
-          timing["time_to_bathroom"] = room.Room_visisted - room_list[0].Room_visisted
-          timing["time_in_bathroom"] = room_list[-1].Room_visisted
+          if(room_list[index].Room_visited != 0 and room_list[index+1].Room_visited - room_list[index].Room_visited < 300 and room_list[index+1].Room_visited > room.Room_visited): # Huske at vurdere der ikke er gået for lang tid siden man gik i midterste rum 
+            break
+        
+        elif(room_list[index-room_list_len].Room_visited != 0):
+          
+          timing["time_in_bathroom"] = room_list[-2].Room_visited - timing["time_in_bathroom"]
+          timing["time_to_bedroom"] = room_list[0].Room_visited - room_list[-2].Room_visited
+          self.MYSQL_Server.store_bathroom_event(timing)
     else:
+      if(room_list[0].Room_visited < room_list[-1].Room_visited and room_list[0].Room_visited != 0):
+       
+        timing["start_time"] = room_list[0].Room_visited
+        timing["time_to_bathroom"] = room_list[-1].Room_visited - room_list[0].Room_visited
+        timing["time_in_bathroom"] = room_list[-1].Room_visited
+        room_list[0].Room_visited = 0
       for index, room in enumerate(room_list):
         if(index != room_list_len):
-          if(room_list[-1].Room_visisted != 0 and room_list[-1-index].Room_visisted - room.Room_visisted < 300 and room_list[-1-index].Room_visisted > room.Room_visisted):
-            pass
-        elif(room_list[index-room_list_len].Room_visisted != 0):
-          timing["time_in_bathroom"] = room_list[-2].Room_visisted - timing["time_in_bathroom"]
-          timing["time_to_bedroom"] = room_list[0].Room_visisted - room_list[-2].Room_visisted
-    if(all(times != 0 for times in timing.values())):
-      self.MYSQL_Server.store_bathroom_event(timing)
+          #if(room_list[-1].Room_visited != 0 and room_list[index])
+          if(room_list[-1].Room_visited != 0 and room_list[-2-index].Room_visited - room_list[-1-index].Room_visited < 300 and room_list[-1-index].Room_visited > room.Room_visited):
+            break
+        
+        
+    #for room in room_list:
+    #  print(room.Room_visited)
+    #if(all(times != 0 for times in timing.values())):
+      
 
 
     #start tid = Tid_sensor1
@@ -138,11 +151,12 @@ class Controller(): # changes model aka rooms
           #client.publish(topic=f"zigbee2mqtt/LED{payload_recieve["SENSOR_ID"]}/set", payload=json.dumps({"state": "ON"}))
           client.publish(topic=f"zigbee2mqtt/LED1/set", payload=json.dumps({"state": "OFF"}))
           self.MYSQL_Server.store_sensor_event(room_num_index+1)
+          self.room_list[room_num_index].Room_visited = int(time.time())
+
           self.bathroom_event(self.room_list, timing)
-          self.room_list[room_num_index].Room_visisted = int(time.time())
-        elif (self.room_list[room_num_index].Room_visisted != 0 and  int(time.time()) - self.room_list[room_num_index].Room_visisted > 5):
+        elif (self.room_list[room_num_index].Room_visited != 0 and  int(time.time()) - self.room_list[room_num_index].Room_visited > 5):
           client.publish(topic=f"zigbee2mqtt/LED1/set", payload=json.dumps({"state": "ON"}))
-          self.room_list[room_num_index].Room_visisted  = 0 ### måske ændres
+          self.room_list[room_num_index].Room_visited  = 0 ### måske ændres
 
   def control_loop(self):
     # Create a data model and add a list of known Zigbee devices.
@@ -181,14 +195,13 @@ class Controller(): # changes model aka rooms
       sleep(3)
       mqtt_client.publish(topic="zigbee2mqtt/SENSOR2", payload=json.dumps({"occupancy": "true"}))
       sleep(3)
-      mqtt_client.publish(topic="zigbee2mqtt/SENSOR1", payload=json.dumps({"occupancy": "true"}))    
+      mqtt_client.publish(topic="zigbee2mqtt/SENSOR1", payload=json.dumps({"occupancy": "true"})) 
+      
+      sleep(3)
+      print("DONE WITH SENDING MESSAGES")   
       while True:
         print(f"Waiting ...{x}")
         sleep(2)
-        
-          
-        print(self.room_list)
-        print(timing)
     except KeyboardInterrupt:
       print("Oh! you pressed CTRL + C.")
       print("Program interrupted.")
